3f6f9cfbae870aff07b6d77fdb0a88faa6ae6c4f
falcosan <danielefalche@gmail.com>
2021-04-06 23:59:25 +0200

    dev

--- src/components/HeaderComponentVue.vue
+++ src/components/HeaderComponentVue.vue
@@ -2,7 +2,7 @@
   <header class="header top-0 z-10 fixed h-20 w-auto justify-between mt-0">
     <nav class="navbar">
       <div class="navbar_menu">
-<router-link></router-link>
+        <router-link to="/">Home</router-link>
       </div>
     </nav>
   </header>
