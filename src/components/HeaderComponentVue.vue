c703fa841929c82a575a08550687c9dd7001c3c0
falcosan <danielefalche@gmail.com>
2021-04-06 23:48:31 +0200

    dev

---
+++ src/components/HeaderComponentVue.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header top-0 z-10 fixed h-20 w-auto justify-between mt-0">
+    <nav class="navbar">
+      <div class="navbar_menu">
+<router-link></router-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
